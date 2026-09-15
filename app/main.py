7fa0ef8dfd758bdf38dc92a613e431a64d5ab65f
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2026-09-15 12:29:14 +0200

    refactored  add newline and improve code formatting in main.py

--- app/main.py
+++ app/main.py
@@ -2,3 +2,7 @@ from fastapi import FastAPI
 
 from .database import Base, engine
 from .routers.payment_links import router as payment_links_router
+
+
+# Create database tables
+Base.metadata.create_all(bind=engine)
