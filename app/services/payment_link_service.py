3cc8a9f062e4f6ae4a68e694a432ea5702df925b
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2026-09-15 12:21:03 +0200

    working on the payment link business logic

---
+++ app/services/payment_link_service.py
@@ -0,0 +1,6 @@
+# This is where i work on generaoing the payment link for the user to pay for the order. I will be using the payment gateway API to generate the payment link and return it to the user.
+# calaculating expriation 
+
+#creates the payment link
+#saves it to SQLite
+#returns the created record
