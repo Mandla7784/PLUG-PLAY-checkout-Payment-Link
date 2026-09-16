13cf9ae4385364ea45bba25a6012565fb0d27b05
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2026-09-16 10:08:46 +0200

    added middleware

--- app/main.py
+++ app/main.py
@@ -15,6 +15,19 @@ app = FastAPI(
     version="0.1.0",
 )
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/health")
 def health_check():
     return {
