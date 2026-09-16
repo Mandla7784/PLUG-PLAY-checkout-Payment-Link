ba66a71f11178b06feefebcfaecbd800b6e1c4c9
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2026-09-16 09:34:42 +0200

    refactored  authentication to use APIKeyHeader for API key dependency

--- app/auth.py
+++ app/auth.py
@@ -1,4 +1,5 @@
-from fastapi import Depends, Header, HTTPException, status
+from fastapi import Depends, HTTPException, status
+from fastapi.security import APIKeyHeader
 from sqlalchemy.orm import Session
 
 from .database import get_db
@@ -8,8 +9,13 @@ from .services.api_key_service import get_merchant_by_api_key
 
 
 # for reusable authentication dependency
+
+api_key_header = APIKeyHeader(
+    name="X-API-Key",
+    auto_error=True,
+)
 def get_current_merchant(
-    x_api_key: str = Header(..., alias="X-API-Key"),
+  x_api_key: str = Depends(api_key_header),
     db: Session = Depends(get_db),
 ) -> Merchant:
     merchant = get_merchant_by_api_key(db, x_api_key)
